752147178567a1115215d683ba5dbe1adf9b0979
Hugo Marx <marx.hugo@gmail.com>
2022-04-21 15:11:27 +0200

    Bases sécurisation serveur pour le form backoffice

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -9,9 +9,24 @@ class BackOfficeController extends AbstractController
         return $this->twig->render('Back_office/dashboard.html.twig');
     }
 
-    public function add(): string
+    public function add()
     {
 
-        return $this->twig->render('Back_office/add_item.html.twig');
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            var_dump($_POST);
+            $error = [];
+            foreach ($_POST as $key => $value) {
+                if (!$value) {
+                    $error[] = $key;
+                }
+            }
+            if (count($error) === 0) {
+                return $this->twig->render('Back_office/dashboard.html.twig');
+            } else {
+                return  $this->twig->render('Back_office/add_item.html.twig',['error' => $error]);
+            }
+        } else {
+            return $this->twig->render('Back_office/add_item.html.twig');
+        }
     }
 }
