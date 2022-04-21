b4b79ac0748b4a829130ecb2ad781383ffa5f24d
Hugo Marx <marx.hugo@gmail.com>
2022-04-21 17:33:47 +0200

    modif route dashboard

--- src/routes.php
+++ src/routes.php
@@ -13,6 +13,6 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
-    'backoffice/dashboard' => ['BackOfficeController', 'dashboard'],
+    'backoffice' => ['BackOfficeController', 'dashboard'],
     'backoffice/add' => ['BackOfficeController', 'add'],
 ];
