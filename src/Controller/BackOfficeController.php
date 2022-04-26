652fb5f8c780519bba21c7d97e7da1a4906cbb7e
Hugo Marx <marx.hugo@gmail.com>
2022-04-26 17:06:54 +0200

    Dependances check

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -20,7 +20,7 @@ class BackOfficeController extends AbstractController
             var_dump($_POST);
             var_dump($_FILES);
             $fieldError = [];
-            $fileError = [];
+            $fileError = $this->fileCheck();
 
             foreach ($_POST as $key => $value) {
                 if (!$value) {
@@ -28,39 +28,45 @@ class BackOfficeController extends AbstractController
                 }
             }
 
-           if (!is_numeric($_POST['prix'])) {
+            if (!is_numeric($_POST['prix']) && !empty($_POST['prix'])) {
                 $fieldError[] = 'Le prix doit être un chiffre';
             }
 
-            if (!empty($_FILES['images']['name'])) {
-                $file = uniqid() . $_FILES['images']['name'];
-                //echo $file;
-                $uploadDir = 'C:\Users\hugo_dev\Desktop\upload_test\\';
-                $uploadPath = $uploadDir . $file;
-                $valideExtensions = ['jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG', 'webp'];
-                $fileExtension = pathinfo($_FILES['images']['name'], PATHINFO_EXTENSION);
-                echo $fileExtension;
-                $maxFileSize = 2097152;
-
-                if ($_FILES['images']['size'] > $maxFileSize) {
-
-                    $fileError[] = 'Le fichier ne peut dépasser 2MB';
-
-                } else if (!in_array($fileExtension, $valideExtensions)) {
-
-                    $fileError[] =  'Cette extension de fichier n\'est pas prise en charge';
-                }
-
-            }
 
             if ($_FILES['images']['error'] === 0 && count($fieldError) === 0 && count($fileError) === 0) {
+                $file = uniqid() . $_FILES['images']['name'];
+                $uploadDir = 'C:\Users\hugo_dev\Desktop\upload_test\\';
+                $uploadPath = $uploadDir . $file;
                 move_uploaded_file($_FILES['images']['tmp_name'], $uploadPath);
-               // echo  $message = 'Upload successfull';
-                return $this->twig->render('Back_office/add_item.html.twig');
+                $fileIsValid = true;
+                return $this->twig->render('Back_office/add_item.html.twig', ['fileIsValid' => $fileIsValid]);
             } else {
-                return  $this->twig->render('Back_office/add_item.html.twig', ['fieldError' => $fieldError, 'fileError' => $fileError]);
+                return  $this->twig->render(
+                    'Back_office/add_item.html.twig',
+                    ['fieldError' => $fieldError, 'fileError' => $fileError]
+                );
             }
         }
         return $this->twig->render('Back_office/add_item.html.twig');
+    }
+
+    private function fileCheck(): array
+    {
+        $fileError = [];
+        if (!empty($_FILES['images']['name'])) {
+            $valideExtensions = ['jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG', 'webp'];
+            $fileExtension = pathinfo($_FILES['images']['name'], PATHINFO_EXTENSION);
+            $maxFileSize = 2097152;
+
+            if ($_FILES['images']['size'] > $maxFileSize) {
+                $fileError[] = 'Le fichier ne peut dépasser 2MB';
+            }
+
+            if (!in_array($fileExtension, $valideExtensions)) {
+                $fileError[] =  'Cette extension de fichier n\'est pas prise en charge';
+            }
+        }
+
+        return $fileError;
     }
 }
