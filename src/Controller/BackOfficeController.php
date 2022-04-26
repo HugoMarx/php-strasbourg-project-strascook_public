606820a7d1d9b04566c8ea46fa7a07ccbd31f49f
Hugo Marx <marx.hugo@gmail.com>
2022-04-26 17:22:54 +0200

    Suppression var_dump

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -17,8 +17,6 @@ class BackOfficeController extends AbstractController
     {
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            var_dump($_POST);
-            var_dump($_FILES);
             $fieldError = [];
             $fileError = $this->fileCheck();
 
