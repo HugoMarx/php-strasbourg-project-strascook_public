1edfb6a4b56e151eac83eed30766cf9d4d9cc7b1
SULEYMAN KUTUK <kutuk.suleymann@gmail.com>
2022-05-04 16:42:15 +0200

    message de confirmation si form ok

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -6,14 +6,6 @@ class FormController extends AbstractController
 {
     public function index()
     {
-
-        return $this->twig->render('Form/index.html.twig');
-    }
-
-
-
-    public function new()
-    {
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $errors = [];
             if (empty($_POST["nom"] || empty($_POST["prenom"]))) {
@@ -35,13 +27,36 @@ class FormController extends AbstractController
                 $errors['message'] = "Veuillez remplir le champ message";
             }
 
-            if ($errors) {
-                return $this->twig->render('Form/index.html.twig', [
-                    'errors' => $errors
-                ]);
+            if (!$errors) {
+                $nom = htmlentities($_POST['nom']);
+                $prenom = htmlentities($_POST['prenom']);
+                $adresse = htmlentities($_POST['adresse']);
+                $postal = htmlentities($_POST['postal']);
+                $email = htmlentities($_POST['email']);
+                $message = htmlentities($_POST['message']);
+
+
+
+                $destinataire = '@gmail.com';
+                $contenu = '<p>Tu as un nouveau message !</p>';
+                $contenu .= '<p><strong>Nom</strong>: ' . $nom . '</p>';
+                $contenu .= '<p><strong>Email</strong>: ' . $prenom . '</p>';
+                $contenu .= '<p><strong>Message</strong>: ' . $adresse . '</p>';
+                $contenu .= '<p><strong>Message</strong>: ' . $postal . '</p>';
+                $contenu .= '<p><strong>Message</strong>: ' . $email . '</p>';
+                $contenu .= '<p><strong>Message</strong>: ' . $message . '</p>';
+
+
+                mail($destinataire, 'test', $contenu);
+                header("location: /form?message=ok");
             }
+
+            return $this->twig->render('Form/index.html.twig', [
+                'errors' => $errors,
+                'data' => $_POST
+            ]);
         }
 
-        header("Location: /form");
+        return $this->twig->render('Form/index.html.twig', ['message' => isset($_GET['message'])]);
     }
 }
