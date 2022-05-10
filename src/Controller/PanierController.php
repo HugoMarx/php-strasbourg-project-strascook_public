bef0b7a99291f774435ab3cc4c46402e55bcdb0a
SULEYMAN KUTUK <kutuk.suleymann@gmail.com>
2022-05-10 13:28:30 +0200

    fonctionmail

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -99,16 +99,71 @@ class PanierController extends AbstractController
                 $_SESSION['user_details']['number'] = $_POST['number'];
                 header('Location: /panier/order_recap');
             } else {
-                return $this->twig->render('/Reservation/contact_form.html.twig', ['field_error' => $emptyFieldError]);
+                return $this->twig->render(
+                    '/Reservation/contact_form.html.twig',
+                    ['field_error' => $emptyFieldError]
+                );
             }
         }
         return $this->twig->render('/Reservation/contact_form.html.twig');
     }
 
+
+    public function mailto()
+    {
+
+        $panier = '<table>';
+        $panier .= '<thead>';
+        $panier .= '<tr>';
+        $panier .= '<td>Plat</td>';
+        $panier .= '<td>price</td>';
+        $panier .= '<td>qte</td>';
+        $panier .= '</tr>';
+        $panier .= '</thead>';
+        $panier .= '<tbody>';
+        foreach ($_SESSION['cart'] as $product) {
+            $panier .= '<tr>';
+            $panier .= '<td>' . $product['name'] . '</td>';
+            $panier .= '<td>' . $product['price'] . '</td>';
+            $panier .= '<td>' . $product['qte'] . '</td>';
+            $panier .= '</tr>';
+        }
+        $panier .= '</tbody>';
+        $panier .= '</table>';
+
+        $entete  = 'MIME-Version: 1.0' . "\r\n";
+        $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        $entete .= 'From: strascook@monsite.fr' . "\r\n";
+        $entete .= 'Reply-to: strascook@monsite.fr';
+
+        $destinataire = 'kutuk.suleymann@gmail.com';
+        $contenu = ' Merci nous avons bien recu votre commande !' . '<br>';
+        $contenu .= 'Nom: ' . $_SESSION['user_details']['lastname'] . '<br>';
+        $contenu .= 'Prenom: ' . $_SESSION['user_details']['firstname'] . '<br>';
+        $contenu .= 'Adresse: ' . $_SESSION['user_details']['city'] . '<br>';
+        $contenu .= 'Code-Postal: ' . $_SESSION['user_details']['post_code'] . '<br>';
+        $contenu .= 'E-mail: ' . $_SESSION['user_details']['email'] . '<br>';
+        $contenu .= 'Message: <br/>' . $panier . '<br>';
+        $contenu .= "<img 
+            src='https://i.ibb.co/FsLK0CW/Logo-Strascook-Alpha.png' width='300px' height='150px'/>";
+
+        mail($destinataire, 'Strascook', $contenu, $entete);
+
+        header('Location: /panier/order_recap?message=ok');
+    }
+
+
+
+
+
     public function validation()
     {
         return $this->twig->render('Panier/validation.html.twig');
     }
+
+
+
+
 
     public function orderRecap()
     {
@@ -118,7 +173,8 @@ class PanierController extends AbstractController
             'Panier/recap.html.twig',
             [
                 'total_price' => $totalPrice,
-                'total_item' => $totalItem            ]
+                'total_item' => $totalItem
+            ]
         );
     }
 }
